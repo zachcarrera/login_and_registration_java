0c19028fe4769a5d9793e3c1ce8a7ad5fe801bf6
Zach Carrera <zachcarrera@gmail.com>
2023-04-14 12:10:43 -0700

    update LoginUser with email and password attributes

--- src/main/java/com/zachcarrera/loginandregistration/models/LoginUser.java
+++ src/main/java/com/zachcarrera/loginandregistration/models/LoginUser.java
@@ -1,5 +1,39 @@
 package com.zachcarrera.loginandregistration.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class LoginUser {
+
+	@NotEmpty
+	@Email
+	private String email;
+
+	// password
+	@NotEmpty
+	@Size(min = 8, max = 128)
+	private String password;
+
+	// constructor
+	public LoginUser() {
+	}
+
+	// getters and setters
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
